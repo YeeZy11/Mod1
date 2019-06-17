322028de1d2e643224faa83404245e58be4e8c47
unknown <pedro_dutra@saojose.sc.senai.br>
2019-06-17 08:50:05 -0300

    Lista

---
+++ mod3/EX/todo.js
@@ -0,0 +1,7 @@
+var listElement = document.querySelector("#app ul");
+var listImput = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+console.log(listElement);
+console.log(buttonElement);
+console.log(listImput);
